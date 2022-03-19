ad4127d2684abe918b642fae6abc6ab542b87aa9
Jake Morris <Jakeamorr@gmail.com>
2022-03-19 15:51:09 -0400

    clean up styling for app component

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,7 +1,9 @@
+import Landing from "./Landing";
+
 export default function SignIn() {
-    return (
-        <div>
-            mock sign in component
-        </div>
-    )
+  return (
+    <div style={{ border: "solid black 2px", height: "10rem", width: "10rem" }}>
+      mock sign in component
+    </div>
+  );
 }
