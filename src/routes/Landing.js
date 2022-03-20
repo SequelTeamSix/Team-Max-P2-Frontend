1320972e08718fcf4705f21274456a928ef4a37a
Jake Morris <Jakeamorr@gmail.com>
2022-03-20 10:29:21 -0400

    refactor some general css and add styling for sign in component

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Landing() {
+  let navigate = useNavigate();
+
   return (
-    <div className="landing-page">
+    <div className="modal">
       <h1 className="company-title">Maxtermind Inc</h1>
       <div className="landing-page-button-container">
-        <button className="landing-page-button">Admin Login</button>
+        {/* TODO: Need to figure out if these should route to separate components */}
+        <button className="button" onClick={() => navigate("/signIn")}>
+          Admin Login
+        </button>
         <hr className="horizontal-break" />
-        <button className="landing-page-button">Employee Login</button>
+        {/* TODO: Need to figure out if these should route to separate components */}
+        <button className="button" onClick={() => navigate("/signIn")}>
+          Employee Login
+        </button>
       </div>
       <p>Terms of use | Privacy Policy</p>
     </div>
--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,9 +1,26 @@
-import Landing from "./Landing";
-
 export default function SignIn() {
   return (
-    <div style={{ border: "solid black 2px", height: "10rem", width: "10rem" }}>
-      mock sign in component
+    <div className="modal">
+      <h1 className="company-title">Maxtermind Inc</h1>
+      <div className="sign-in-input-group">
+        <label>Username</label>
+        <input type="text"></input>
+      </div>
+      <div className="sign-in-input-group">
+        <label>Password</label>
+        <input type="text"></input>
+      </div>
+      <button
+        className="button"
+        onClick={() =>
+          console.log(
+            "add the logic to route to either the admin page or the user page"
+          )
+        }
+      >
+        Login
+      </button>
+      <p>Terms of use | Privacy Policy</p>
     </div>
   );
 }
