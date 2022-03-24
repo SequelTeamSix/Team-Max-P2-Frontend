662f18ad04f00332ff5500af7c1ba1f76be457e6
Jake Morris <Jakeamorr@gmail.com>
2022-03-24 13:32:24 -0400

    add styling

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,7 +4,7 @@ export default function Notification(props) {
       className="notification"
       onClick={() => props.clearNotification(props.id)}
     >
-      <p>
+      <p style={{ marginBottom: 0 }}>
         {props.date} | {props.action} | {props.position.name}
       </p>
     </div>
